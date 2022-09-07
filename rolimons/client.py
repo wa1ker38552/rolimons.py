fb9cbe299bc80e36d20c240b82cefee0c458636f
walker <100868154+wa1ker38552@users.noreply.github.com>
2022-09-07 09:02:39 -0700

    Created client.update_wishlist() function

--- rolimons/client.py
+++ rolimons/client.py
@@ -3,4 +3,10 @@ import requests
 class Client:
   def __init__(self, token):
     self.client = requests.Session()
-    self.client.cookies['_RoliVerification'] = token
+    self.token = token
+
+  def update_wishlist(self, items):
+    if not isinstance(items, list): items = [items]
+    data = {'asset_ids': items}
+    request = self.client.post('https://www.rolimons.com/tradesettingsapi/updatewishlist', json=data, headers={'cookie': self.token})
+    return request.status_code
