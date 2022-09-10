acb5e28143b640523e0211f684a1c54b02a85850
walker <100868154+wa1ker38552@users.noreply.github.com>
2022-09-10 11:36:21 -0700

    Update to add post_trade_ad()

--- rolimons/client.py
+++ rolimons/client.py
@@ -3,14 +3,31 @@ import requests
 class Client:
   def __init__(self, token):
     self.client = requests.Session()
-    self.token = token
+    self.client.headers.update({'cookie': token})
+
+  def post_trade_ad(self, offering, request, id):
+    # require id since there is no way to find id from cookie
+    request_ids, request_tags = [], []
+    for item in request:
+      try:
+        request_ids.append(int(item))
+      except ValueError:
+        request_tags.append(item)
+    data = {
+      'offer_item_ids': offering,
+      'player_id': id,
+      'request_item_ids': request_ids,
+      'request_tags': request_tags
+    }
+    request = self.client.post('https://www.rolimons.com/tradeapi/create', json=data)
+    return request.status_code
 
   def update_wishlist(self, items):
     if not isinstance(items, list): items = [items]
     data = {'asset_ids': items}
-    request = self.client.post('https://www.rolimons.com/tradesettingsapi/updatewishlist', json=data, headers={'cookie': self.token})
+    request = self.client.post('https://www.rolimons.com/tradesettingsapi/updatewishlist', json=data)
     return request.status_code
-  
+
   def update_asking(self, items):
     # format
     # {item: [tags, ...]}
@@ -22,7 +39,7 @@ class Client:
         data['assets'][i][option] = True
     request = self.client.post('https://www.rolimons.com/tradesettingsapi/updateaskinglist', json=data)
     return request.status_code
-  
+
   def add_player(self, username):
     request = self.client.post(f'https://www.rolimons.com/playerapi/addplayer?playername={username}')
     return request.status_code
